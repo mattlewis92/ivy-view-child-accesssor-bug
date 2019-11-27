c22228e4ccc688b1b6967f8d36a72b9ad27db1bc
Matt Lewis <hello@mattlewis.me>
2019-11-27 15:46:15 +0000

    Add minimal repro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FooComponent } from './foo/foo.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild(FooComponent) foo: FooComponent
+
   title = 'ivy-view-child-accesssor-bug';
 }
